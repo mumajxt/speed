a4b78554e76ea7239d2f2507a0618c142854beeb
oyaji <1491822925@qq.com>
2021-10-14 10:36:10 +0800

    宠物皮肤添加问题

--- speed_server/src/main/java/com/jxt/dataspeed/config/TomcatConfig.java
+++ speed_server/src/main/java/com/jxt/dataspeed/config/TomcatConfig.java
@@ -11,38 +11,38 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class TomcatConfig {
-//    @Value("${server.custom.httpPort}")
-//    private Integer httpPort;
-//
-//    @Value("${server.port}")
-//    private Integer httpsPort;
-//
-//    @Bean
-//    public TomcatServletWebServerFactory servletContainer() { //springboot2 新变化
-//
-//        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory() {
-//
-//            @Override
-//            protected void postProcessContext(Context context) {
-//
-//                SecurityConstraint securityConstraint = new SecurityConstraint();
-//                securityConstraint.setUserConstraint("CONFIDENTIAL");
-//                SecurityCollection collection = new SecurityCollection();
-//                collection.addPattern("/*");
-//                securityConstraint.addCollection(collection);
-//                context.addConstraint(securityConstraint);
-//            }
-//        };
-//        tomcat.addAdditionalTomcatConnectors(initiateHttpConnector());
-//        return tomcat;
-//    }
-//
-//    private Connector initiateHttpConnector() {
-//        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
-//        connector.setScheme("http");
-//        connector.setPort(httpPort);
-//        connector.setSecure(false);
-//        connector.setRedirectPort(httpsPort);
-//        return connector;
-//    }
+    @Value("${server.custom.httpPort}")
+    private Integer httpPort;
+
+    @Value("${server.port}")
+    private Integer httpsPort;
+
+    @Bean
+    public TomcatServletWebServerFactory servletContainer() { //springboot2 新变化
+
+        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory() {
+
+            @Override
+            protected void postProcessContext(Context context) {
+
+                SecurityConstraint securityConstraint = new SecurityConstraint();
+                securityConstraint.setUserConstraint("CONFIDENTIAL");
+                SecurityCollection collection = new SecurityCollection();
+                collection.addPattern("/*");
+                securityConstraint.addCollection(collection);
+                context.addConstraint(securityConstraint);
+            }
+        };
+        tomcat.addAdditionalTomcatConnectors(initiateHttpConnector());
+        return tomcat;
+    }
+
+    private Connector initiateHttpConnector() {
+        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+        connector.setScheme("http");
+        connector.setPort(httpPort);
+        connector.setSecure(false);
+        connector.setRedirectPort(httpsPort);
+        return connector;
+    }
 }
